6d9f204c0b30dec6ee201a6d1a6d800007510eba
hieuvm <hieuvm@vng.com.vn>
2017-11-01 13:31:27 +0700

    Connect UI to code

--- todoapp/todoapp/ViewController.swift
+++ todoapp/todoapp/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mNameLabel: UILabel!
+    @IBOutlet weak var mNameTextFiled: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    @IBAction func onOKClick(_ sender: UIButton) {
+        print("on click")
+        mNameLabel.text = "Default Name"
+        mNameLabel.textColor = UIColor.red
+    }
+    
+    @IBAction func onCancelClick(_ sender: UIButton) {
+        print("cancel click")
+        mNameLabel.textColor = UIColor.blue
+        mNameTextFiled.text = "String"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
