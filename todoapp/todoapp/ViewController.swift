f75e5998327348e7512e19bbb5a36d6474f7a709
hieuvm <hieuvm@vng.com.vn>
2017-11-01 11:53:39 +0700

    First commit

---
+++ todoapp/todoapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  todoapp
+//
+//  Created by Hiếu Vũ Mạnh on 11/1/17.
+//  Copyright © 2017 Hiếu Vũ Mạnh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
